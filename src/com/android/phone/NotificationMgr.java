59009b621db33e4bd44b8476dd19ea87cfddfc04
htchoi1995 <htchoi1995@gmail.com>
2013-06-25 22:41:00 +0900

    Revert "Missed Call Breath (1/3)"
    This reverts commit 9b728d5718b001a6a17dee61d9c74fc9c4eeecfd.
    
    Conflicts:
    	src/com/android/phone/NotificationMgr.java

--- src/com/android/phone/NotificationMgr.java
+++ src/com/android/phone/NotificationMgr.java
@@ -593,14 +593,6 @@ public class NotificationMgr implements CallerInfoAsyncQuery.OnQueryCompleteList
         mMissedCalls.add(0, new MissedCallInfo(callName, number, date));
 
         Notification.Builder builder = new Notification.Builder(mContext);
-        if (Settings.System.getInt(mContext.getContentResolver(),
-               Settings.System.MISSED_CALL_BREATH, 0) == 1) {
-             builder.setSmallIcon(R.drawable.stat_notify_missed_call_breath)
-                .setTicker(mContext.getString(R.string.notification_missedCallTicker, callName))
-                .setWhen(date)
-                .setContentIntent(PendingIntent.getActivity(mContext, 0, callLogIntent, 0))
-                .setAutoCancel(true)
-                .setDeleteIntent(createClearMissedCallsIntent());
            } else {
              builder.setSmallIcon(android.R.drawable.stat_notify_missed_call)
                 .setTicker(mContext.getString(R.string.notification_missedCallTicker, callName))
